e07802cb88c6169303fd41a5a86ca7ba6c85fe7f
Bhanu <bhanujarpula9@gmail.com>
2023-07-03 22:11:12 +0530

    resolved warning

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TL-IITH-2023/",
-})
+  base: '/TL-IITH-2023/',
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB or set it to 0 to disable the warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Put modules from node_modules in the 'vendor' chunk
+          }
+        },
+      },
+    },
+  },
+});
