4e96529d58ec33e580406b220772f912b265f362
Bhanu <bhanujarpula9@gmail.com>
2023-07-03 21:08:07 +0530

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TL-IITH-2023/',
+  base: "/TL-IITH-2023/",
 })
