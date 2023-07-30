066502854ab0ebc0941bd42081ea97db31885aa7
Bhanu <bhanujarpula9@gmail.com>
2023-07-30 15:39:00 +0530

    changing base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TL-IITH-2023/',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB or set it to 0 to disable the warning
     rollupOptions: {
